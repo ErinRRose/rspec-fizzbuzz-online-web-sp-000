7b4abc6ccf24f0a7a0c28efb1faea96ae256776c
Erin  Rose <errose10@gmail.com>
2019-10-23 00:23:12 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,12 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz
-if int % 3 == 0
+  if int % 3 == 0
   puts "Fizz"
 elseif int % 5 == 0
   puts "Buzz"
 elseif int % 3 && 5 == 0
-puts "FizzBuzz"
-else int !% 3 || 5
-end
+  puts "FizzBuzz"
+  else int !% 3 || 5
+    end
 end
