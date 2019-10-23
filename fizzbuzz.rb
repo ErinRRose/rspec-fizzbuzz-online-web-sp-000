699172d8bc7720784a4626d97979bdbea525cd9e
Erin  Rose <errose10@gmail.com>
2019-10-23 00:55:32 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-  puts "Fizz"
+  return "Fizz"
 elsif int % 5 == 0
-  puts "Buzz"
+  return "Buzz"
 elsif int % 3 && 5 == 0
-  puts "FizzBuzz"
+  return "FizzBuzz"
   else int !% 3 || 5
   end
 end
